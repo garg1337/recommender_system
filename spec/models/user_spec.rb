73d88027248ec622b70301dd0b66ed353d143932
Arjun Garg <arjungarg@ubuntu.(none)>
2013-06-15 02:15:00 -0700

    Fixed some stuff

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -59,6 +59,16 @@ describe User do
 		it {should_not be_valid}
 	end
 
+	describe "email address with mixed case" do
+		let(:mixed_case_email) {"FoO@exAMple.CoM"}
+
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			@user.reload.email.should == mixed_case_email.downcase
+		end
+	end
+
 
 	describe "when password is not present" do
 		before { @user.password = @user.password_confirmation = " "}
@@ -79,6 +89,7 @@ describe User do
 		before { @user.password = @user.password_confirmation = "a"*5}
 		it	{should_not be_valid}
 	end
+
 
 	describe "return value of authenticate method" do
 		before { @user.save}
