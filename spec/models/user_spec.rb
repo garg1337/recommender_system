a545f580b37773074b96a4220e20be9f33f33942
Arjun Garg <arjungarg@ubuntu.(none)>
2013-06-16 03:12:51 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,8 @@ describe User do
 	it{ should respond_to(:name)}
 	it{ should respond_to(:email)}
 	it{ should respond_to(:password_digest)}
+	it{ should respond_to(:password)}
+	it{ should respond_to(:remember_token)}
 	it{ should respond_to(:password_confirmation)}
 	it{ should respond_to(:authenticate)}
 	
@@ -103,6 +105,11 @@ describe User do
 			it {should_not == user_for_invalid_password}
 		end
 
+	end
+
+	describe "remember token" do
+		before {@user.save}
+		its(:remember_token){should_not be_blank}
 	end
 
 end
